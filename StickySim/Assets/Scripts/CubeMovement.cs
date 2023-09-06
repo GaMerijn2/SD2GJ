4b504689b4139268b8d2d2834e8bb700ad12798b
merijn florie <merijn@florie.nl>
2023-09-06 13:29:26 +0200

    Movement Fixed X / Z

--- StickySim/Assets/Scripts/CubeMovement.cs
+++ StickySim/Assets/Scripts/CubeMovement.cs
@@ -4,39 +4,37 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Rigidbody rb;
-    Vector3 posY;
+
     bool isStopped = false;
     void Start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
         Debug.Log("Start");
         isStopped = false;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (transform.position.x >= -5 && !isStopped)
+        tileMovement();
+    }
+     private void tileMovement()
+    {
+        if (this.gameObject.CompareTag("Left") && transform.position.x >= -5 && !isStopped)
         {
             transform.position += new Vector3(3, 0, 0) * Time.deltaTime;
-           // Debug.Log(transform.position.x);
+            // Debug.Log(transform.position.x);
         }
 
-        else if (transform.position.z >= 5 && !isStopped)
+        if (this.gameObject.CompareTag("Right") && transform.position.z <= 5 && !isStopped)
         {
             transform.position += new Vector3(0, 0, -3) * Time.deltaTime;
-           // Debug.Log(transform.position.z);
+             Debug.Log(transform.position.z);
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-           // transform.position = this.transform.position;
-           isStopped = true;
-            Debug.Log("Stop");
+            // transform.position = this.transform.position;
+            isStopped = true;
+            // Debug.Log("Stop");
         }
-
     }
 }
